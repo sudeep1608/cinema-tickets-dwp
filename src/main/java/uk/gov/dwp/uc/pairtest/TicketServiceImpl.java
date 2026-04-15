0d4282d08db65252b47195e6b5d22f233a6dd20a
Sudeep Suresh <ssuresh2@sheffield.ac.uk>
2026-04-15 13:02:25 +0100

    feat: add ticket aggregation logic using Map and streams

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -5,6 +5,10 @@ import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class TicketServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
@@ -22,9 +26,17 @@ public class TicketServiceImpl implements TicketService {
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
 
-        //entry point for processing ticket purchases.
-        //implementation will be built ( validation, calculation and external calls)
+        // Group ticket requests by type and sum their quantities.
+        Map<TicketTypeRequest.Type,Integer> ticketCounts =
 
+                Arrays.stream(ticketTypeRequests)
+                        .collect(
+                                Collectors.groupingBy(
+                                        TicketTypeRequest::getTicketType,
+                                        java.util.stream.Collectors.summingInt(TicketTypeRequest::getNoOfTickets)
+                                ));
+
+        //validation of the request needs to be added before processing
     }
 
 }
